b64734c4b7d1b3f56458e0f6a34780acbee35f7e
Artem Tovbin <arty99@gmail.com>
2020-12-01 13:25:57 +0300

    Lesson 2 Homework + date_from/to. Take some code from trainer

--- magnit_parse.py
+++ magnit_parse.py
@@ -8,7 +8,22 @@ import dotenv
 from urllib.parse import urljoin
 
 dotenv.load_dotenv('.env')
-
+dotenv.load_dotenv('.env')
+MONTHS = {
+    "янв": 1,
+    "фев": 2,
+    "мар": 3,
+    "апр": 4,
+    "май": 5,
+    "мая": 5,
+    "июн": 6,
+    "июл": 7,
+    "авг": 8,
+    "сен": 9,
+    "окт": 10,
+    "ноя": 11,
+    "дек": 12,
+}
 
 class MagnitParse:
     headers = {
@@ -19,26 +34,6 @@ class MagnitParse:
         self.start_url = start_url
         client = pymongo.MongoClient(os.getenv('DATA_BASE'))
         self.db = client['gb_parse_11']
-
-# {
-        #     "url": str,
-        #     "promo_name": str,
-        #     "product_name": str,
-        #     "old_price": float,
-        #     "new_price": float,
-        #     "image_url": str,
-        #     "date_from": "DATETIME",
-        #     "date_to": "DATETIME",
-        # }
-        self.product_template = {
-            'url': lambda soup: urljoin(self.start_url, soup.get('href')),
-            'promo_name': lambda soup: soup.find('div', attrs={'class': 'card-sale__header'}).text,
-            'product_name': lambda soup: soup.find('div', attrs={'class': 'card-sale__title'}).text,
-            'old_price': lambda soup: soup.find('div', attrs={'class': 'label__price_old'}).text,
-            'new_price': lambda soup: soup.find('div', attrs={'class': 'label__price_new'}).text,
-            'image_url': lambda soup: urljoin(self.start_url, soup.find('img').get('data-src')),
-            'date_from': lambda soup: soup.find('div', attrs={'class': 'card-sale__date'}).text,
-        }
 
 
     @staticmethod
@@ -70,13 +65,47 @@ class MagnitParse:
 
     def get_product(self, product_soup) -> dict:
 
+        # {
+        #     "url": str,
+        #     "promo_name": str,
+        #     "product_name": str,
+        #     "old_price": float,
+        #     "new_price": float,
+        #     "image_url": str,
+        #     "date_from": "DATETIME",
+        #     "date_to": "DATETIME",
+        # }
+        try:
+            dt_parser = self.date_parse(product_soup.find('div', attrs={'class': 'card-sale__date'}).text)
+        except Exception as e:
+            pass
+
+        product_template = {
+            'url': lambda soup: urljoin(self.start_url, soup.get('href')),
+            'promo_name': lambda soup: soup.find('div', attrs={'class': 'card-sale__header'}).text,
+            'product_name': lambda soup: soup.find('div', attrs={'class': 'card-sale__title'}).text,
+            'old_price': lambda soup: soup.find('div', attrs={'class': 'label__price_old'}).text,
+            'new_price': lambda soup: soup.find('div', attrs={'class': 'label__price_new'}).text,
+            'image_url': lambda soup: urljoin(self.start_url, soup.find('img').get('data-src')),
+            'date_from': lambda _: next(dt_parser),
+            'date_from': lambda _: next(dt_parser),
+        }
+
         result = {}
-        for key, value in self.product_template.items():
+        for key, value in product_template.items():
             try:
                 result[key] = value(product_soup)
             except Exception as e:
                 continue
         return result
+
+    @staticmethod
+    def date_parse(date_string: str):
+        date_list = date_string.replace('с ', '', 1).replace('\n', '').split('до')
+        for date in date_list:
+            temp_date = date.split()
+            yield dt.datetime(year=dt.datetime.now().year, day=int(temp_date[0]), month=MONTHS[temp_date[1][:3]])
+
 
     def save(self, product):
         collection = self.db['magnit_11']
