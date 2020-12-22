2b19a40cb59675a745d6c46289c29fb6699ba331
Artem Tovbin <arty99@gmail.com>
2020-12-22 12:44:05 +0300

    Lesson 6 HW

--- gb_parse/items.py
+++ gb_parse/items.py
@@ -30,3 +30,31 @@ class HHVacancyItem(scrapy.Item):
     description = scrapy.Field()
     skills = scrapy.Field()
     company_url = scrapy.Field()
+
+
+class Insta(scrapy.Item):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    data = scrapy.Field()
+    img = scrapy.Field()
+
+
+class InstaTag(Insta):
+    pass
+
+
+class InstaPost(Insta):
+    pass
+
+
+class InstaUser(Insta):
+    pass
+
+
+class InstaFollow(scrapy.Item):
+    _id = scrapy.Field()
+    date_parse = scrapy.Field()
+    user_name = scrapy.Field()
+    user_id = scrapy.Field()
+    follow_name = scrapy.Field()
+    follow_id = scrapy.Field()
--- gb_parse/pipelines.py
+++ gb_parse/pipelines.py
@@ -8,17 +8,28 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 from pymongo import MongoClient
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
 import os
-from dotenv import load_dotenv
+import dotenv
 
+dotenv.load_dotenv('.env')
 class GbParsePipeline:
     def __init__(self):
-        load_dotenv()
 
-        self.db = MongoClient()[os.getenv('DATA_BASE')]
+        self.db = MongoClient(os.getenv('DATA_BASE'))
 
     def process_item(self, item, spider):
         if spider.db_type == 'MONGO':
             collection = self.db[spider.name]
             collection.insert_one(item)
         return item
+
+class GbImagePipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        for img_url in item.get('images', []):
+           yield Request(img_url)
+
+    def item_completed(self, results, item, info):
+         item['images'] = [itm[1] for itm in results]
+         return item
